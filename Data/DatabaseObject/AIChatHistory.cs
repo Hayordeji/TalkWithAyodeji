c30be7ba67c6fd878b7dac11ee0e4faac1a8a41f
Shoga Ayodeji <ayodejishoga1@gmail.com>
2025-07-09 19:37:05 +0100

    .

---
+++ Data/DatabaseObject/AIChatHistory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TalkWithAyodeji.Data.DatabaseObject
+{
+    public class AIChatHistory
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string ConnectionId { get; set; } = string.Empty;
+        public string? ChatHistory { get; set; }
+    }
+}
