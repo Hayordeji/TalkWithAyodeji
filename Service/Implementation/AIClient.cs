0048a699846d746606d569f21d53480e9077e37d
Shoga Ayodeji <ayodejishoga1@gmail.com>
2025-07-11 14:14:41 +0100

    Login Fix2

---
+++ Service/Implementation/AIClient.cs
@@ -0,0 +1,17 @@
+﻿using TalkWithAyodeji.Service.Interface;
+
+namespace TalkWithAyodeji.Service.Implementation
+{
+    public class AIClient : IAIClient
+    {
+        public Task<string> AskAI(string question, string connectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> InitializeSystemPrompt(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
